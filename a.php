21b99f115d963e471c99b8c330fe15ced7aa6e12
msugdr <99190980+msugdr@users.noreply.github.com>
2023-06-04 21:09:11 +0900

    Update a.php

--- a.php
+++ a.php
@@ -5,6 +5,7 @@
 <?php
 // PHP Data Objects(PDO) Sample Code:
 // 
+    /*
     $serverName = "xana-database.database.windows.net";
         $connectionOptions = array(
         "Database" => "xana_sql", // update me
@@ -23,7 +24,23 @@
      echo ($row['sid'] . " " . $row['name'] . "<br>".PHP_EOL);
     }
     sqlsrv_free_stmt($getResults);   
+    */
     
+    try {
+    $conn = new PDO("sqlsrv:server = tcp:xana-database.database.windows.net,1433; Database = xana_sql", "youna", "ledsql123#");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+ 　 echo "Connection succesfully";
+    $sql = "SELECT * FROM member";
+    $res = $pdo->query($sql);
+    $data = $res->fetchAll();
+    echo $data[0]['sid'] . " " . $data[0]['name'] . "<br>";
+    echo $data[1]['sid'] . " " . $data[1]['name'] . "<br>";
+    echo $data[2]['sid'] . " " . $data[2]['name'] . "<br>";
+}
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
 ?>
 </body>
 </html>
