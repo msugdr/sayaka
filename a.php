0ea85711db547d020a81c545d8940d3f25008d7b
msugdr <99190980+msugdr@users.noreply.github.com>
2023-06-04 16:59:17 +0900

    Update a.php

--- a.php
+++ a.php
@@ -5,6 +5,7 @@ try {
     $conn = new PDO("sqlsrv:server = tcp:xana-database.database.windows.net,1433; Database = xana_sql", "youna", "ledsql123#");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
  　 echo "Connection succesfully<br>";
+    $sql="SELECT * FROM member;";
     $res = $conn->query($sql);
     echo $res;
 }
