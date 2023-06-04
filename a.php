af1dc4382038ba2a2f5ebb169ce33ab43b5e6e84
msugdr <99190980+msugdr@users.noreply.github.com>
2023-06-04 17:24:09 +0900

    Update a.php

--- a.php
+++ a.php
@@ -1,12 +1,14 @@
 <!DOCTYPE html>
 <html>
 <body>
-This is a.php
+    <p>This is a.php</p>
 <?php
 // PHP Data Objects(PDO) Sample Code:
 try {
     $conn = new PDO("sqlsrv:server = tcp:xana-database.database.windows.net,1433; Database = xana_sql", "youna", "ledsql123#");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "<p>SUCCESS</p>"
+    $sql = "SELECT * FROM member;";
 }
 catch (PDOException $e) {
     print("Error connecting to SQL Server.");
