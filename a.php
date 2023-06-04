5f6734c8ffa24f4d4e5a3fe17c4b6c5f9cd8fef6
msugdr <99190980+msugdr@users.noreply.github.com>
2023-06-04 17:13:43 +0900

    Update a.php

--- a.php
+++ a.php
@@ -4,7 +4,6 @@ This is a.php
 try {
     $conn = new PDO("sqlsrv:server = tcp:xana-database.database.windows.net,1433; Database = xana_sql", "youna", "ledsql123#");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
- 　 echo "Connection succesfully";
 }
 catch (PDOException $e) {
     print("Error connecting to SQL Server.");
