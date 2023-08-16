4376ac93cc73825b25660fe1b4b645c9775d527f
msugdr <99190980+msugdr@users.noreply.github.com>
2023-08-16 14:47:12 +0900

    Update index.php

--- index.php
+++ index.php
@@ -3,11 +3,33 @@
   <body>
     <h1>SAYAKA</h1> 
 <?php
-echo "I love you.";
+$comment = $_GET['comment'];
+echo " I send you '" . $comment . "'";
+define('LINE_API_URL'  ,'https://notify-api.line.me/api/notify');
+define('LINE_API_TOKEN','0maOvAs8dtXu8h7eEPMXGk2VqMcj6LEztSP9C7kudOg');
+function post_message($message){
+    $data = http_build_query( [ 'message' => $message ], '', '&');
+    $options = [
+        'http'=> [
+            'method'=>'POST',
+            'header'=>'Authorization: Bearer ' . LINE_API_TOKEN . "\r\n"
+                    . "Content-Type: application/x-www-form-urlencoded\r\n"
+                    . 'Content-Length: ' . strlen($data)  . "\r\n" ,
+            'content' => $data,
+            ]
+        ];
+    $context = stream_context_create($options);
+    $resultJson = file_get_contents(LINE_API_URL, false, $context);
+    $resultArray = json_decode($resultJson, true);
+    if($resultArray['status'] != 200)  {
+        return false;
+    }
+    return true;
+}
+post_message($comment);
 ?>
-    <div>
-      <a href="a.html"><img src="girl.png" width="200px"></a>
-    </div>
-      <a href="a.php">PHP</a>
+<form methode="get" action="index.php">
+  <input type="text" name="comment">
+  <input type="submit" value="PUSH">
   </body>
 </html>
